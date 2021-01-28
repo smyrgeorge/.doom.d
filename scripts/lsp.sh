a45d14391c2a9eaefae9fb8cf0852cedbc227c9f
George Smyrnaios <smyrgeorge@gmail.com>
2021-01-28 19:10:03 +0200

    Set doom theme to doom-one, updated lsp.sh

--- scripts/lsp.sh
+++ scripts/lsp.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# python
+echo -e "Installing python:"
 npm i -g pyright
+pip install black
+
+# bash
+echo -e "\nInstalling bash:"
 npm i -g bash-language-server
+
+# docker
+echo -e "\nInstalling docker:"
 npm i -g dockerfile-language-server-nodejs
 
+# rust
+echo -e "\nInstalling rust:"
 brew install rust-analyzer
 brew upgrade rust-analyzer
+
+# kotlin
+echo -e "\nInstalling kotlin:"
+brew install ktlint
+brew upgrade ktlint
