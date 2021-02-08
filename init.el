97190d8ed8f295594584b92d202fc6a84190e580
George Smyrnaios <smyrgeorge@gmail.com>
2021-02-08 10:59:52 +0200

    Enabled emacs-everywhere.

--- init.el
+++ init.el
@@ -194,6 +194,7 @@
 
        :app
        ;;calendar
+       everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        rss               ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
